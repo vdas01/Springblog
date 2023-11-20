37f26e0839de05429bb5a7459f923ed72b173879
Vishal Das <vdas53073@gmail.com>
2023-11-21 00:48:10 +0530

    added basic directory structure

---
+++ src/test/java/io/mountblue/blogapplication/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mountblue.blogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
