742b0a7ff9d51ef66274f9860d1bbbe95c6d353f
Vishal Das <vdas53073@gmail.com>
2023-11-21 22:46:43 +0530

    updated PostService and PostController

--- src/main/java/io/mountblue/blogapplication/controllers/PostController.java
+++ src/main/java/io/mountblue/blogapplication/controllers/PostController.java
@@ -1,4 +1,46 @@
 package io.mountblue.blogapplication.controllers;
 
+
+import io.mountblue.blogapplication.entity.Post;
+import io.mountblue.blogapplication.entity.Tag;
+import io.mountblue.blogapplication.repository.PostRepository;
+import io.mountblue.blogapplication.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
 public class PostController {
+    PostService postService;
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // get post by id;
+    @GetMapping("/post{postId}")
+    public String retreivedPostById(@PathVariable Integer postId, Model model) {
+        return postService.getPostById(postId,model);
+    };
+
+    @GetMapping("/newpost")
+    public String createNewPost(Model model){
+        return  postService.getNewPost(model);
+    }
+
+    @PostMapping("/createpost")
+    public String processNewPost(@ModelAttribute("post") Post post,@ModelAttribute("tag")Tag tag){
+        return postService.postCreation(post,tag);
+    }
+
+
+
+
+
 }
--- src/main/java/io/mountblue/blogapplication/services/PostService.java
+++ src/main/java/io/mountblue/blogapplication/services/PostService.java
@@ -1,5 +1,67 @@
 package io.mountblue.blogapplication.services;
 
+import io.mountblue.blogapplication.entity.Post;
+import io.mountblue.blogapplication.entity.Tag;
+import io.mountblue.blogapplication.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.Optional;
+
+@Service
 public class PostService {
+
+    private PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public PostService(){}
+
+    public String getPostById(int postId, Model model){
+        Optional<Post> retrievedPostById = postRepository.findById(postId);
+
+        if (retrievedPostById.isPresent()) {
+
+            Post post = retrievedPostById.get();
+            model.addAttribute("inputPost", post);
+
+            System.out.println("retreived post :- ");
+            System.out.println(post.getId());
+            System.out.println(post.getAuthor());
+            System.out.println(post.getExcerpt());
+
+        }
+        return "Post";
+    }
+
+    public String getNewPost(Model model){
+        Post newPost = new Post();
+        Tag newTag = new Tag();
+
+        model.addAttribute("post",newPost);
+        model.addAttribute("tag",newTag);
+
+        return "CreatePost";
+    }
+
+    public String postCreation(Post newPost, Tag newTag){
+        Post post = new Post();
+        post = newPost;
+
+        String[] tagsArray = newTag.getName().split(",");
+        for(String tempTag: tagsArray){
+            Tag tag = new Tag(tempTag);
+            post.addTags(tag);
+        }
+
+        postRepository.save(post);
+        return "redirect:/";
+    }
+
 
 }
